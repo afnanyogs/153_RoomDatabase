03dda544585b83cb541138ebb98aced70296d43a
Afnan Asilah <afnanyogs@gmail.com>
2025-11-28 14:34:18 +0700

    Menambahkan layar EntrySiswaScreen dengan scaffold dan top app bar

---
+++ app/src/main/java/com/example/activity_7/view/HalamanEntry.kt
@@ -0,0 +1,41 @@
+package com.example.activity_7.view
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.activity_7.R
+import com.example.activity_7.view.route.DestinasiEntry
+import com.example.activity_7.viewmodel.DetailSiswa
+import com.example.activity_7.viewmodel.EntryViewModel
+import com.example.activity_7.viewmodel.UIStateSiswa
+import com.example.activity_7.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+   )
+)
