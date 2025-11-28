99134f1abe7a5db8c353afd9a810eb4997b7b524
Afnan Asilah <afnanyogs@gmail.com>
2025-11-28 14:15:49 +0700

    menambahkan RepositoriSiswa.kt

--- app/src/main/java/com/example/activity_7/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/activity_7/repositori/RepositoriSiswa.kt
@@ -1,2 +1,18 @@
 package com.example.activity_7.repositori
 
+import androidx.room.Dao
+import com.example.activity_7.room.Siswa
+import com.example.activity_7.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
