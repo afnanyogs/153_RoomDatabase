d677d98510c00bb59a5ded450e2bfbf2aea5a334
Afnan Asilah <afnanyogs@gmail.com>
2025-11-28 14:17:18 +0700

    menambahkan Siswa.kt

--- app/src/main/java/com/example/activity_7/room/Siswa.kt
+++ app/src/main/java/com/example/activity_7/room/Siswa.kt
@@ -1,11 +1,11 @@
-package com.example.example.activity_7.room
+package com.example.activity_7.room
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tblSiswa")
 data class Siswa(
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = true )
     val id : Int = 0,
     val nama : String,
     val alamat : String,
