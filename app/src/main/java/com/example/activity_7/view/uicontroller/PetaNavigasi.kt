b64dc144af2db7f3a8f33c7b349befb3802b900d
Afnan Asilah <afnanyogs@gmail.com>
2025-11-28 14:27:01 +0700

    Menambahkan SiswaApp sebagai pengatur navigasi utama

---
+++ app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,18 @@
+package com.example.activity_7.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.activity_7.view.EntrySiswaScreen
+import com.example.activity_7.view.HomeScreen
+import com.example.activity_7.view.route.DestinasiEntry
+import com.example.activity_7.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+
+}
