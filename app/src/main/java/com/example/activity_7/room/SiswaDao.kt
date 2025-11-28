90ba44acabbf9af1806d1829bec8fe53d431d81d
Afnan Asilah <afnanyogs@gmail.com>
2025-11-28 14:18:47 +0700

    select SiswaDao.kt

--- app/src/main/java/com/example/activity_7/room/SiswaDao.kt
+++ app/src/main/java/com/example/activity_7/room/SiswaDao.kt
@@ -9,5 +9,9 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface SiswaDao {
 
+    @Query(value = "SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
 }
