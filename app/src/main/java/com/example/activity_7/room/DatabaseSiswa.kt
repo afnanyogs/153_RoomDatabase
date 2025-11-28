4796c56c5c97f6ab4b720af2cf4f6ef7f8fad219
Afnan Asilah <afnanyogs@gmail.com>
2025-11-28 14:16:38 +0700

    menambahkan DatabaseSiswa.kt

--- app/src/main/java/com/example/activity_7/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/activity_7/room/DatabaseSiswa.kt
@@ -4,26 +4,27 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.example.activity_7.room.Siswa
+import com.example.activity_7.room.Siswa
+
+
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
 
-    abstract fun siswaDao() : SiswaDao
+    abstract fun siswaDao(): SiswaDao
 
     companion object {
-
         @Volatile
-        private var Instance: DatabaseSiswa? = null
+        private var INSTANCE: DatabaseSiswa? = null
 
         fun getDatabase(context: Context): DatabaseSiswa {
-            return (Instance ?: synchronized(lock = this) {
+            return INSTANCE ?: synchronized(this) {
                 Room.databaseBuilder(
                     context,
-                    klass = DatabaseSiswa::class.java,
-                    name = "siswa_database")
-                    .build().also { Instance = it }
-            })
+                    DatabaseSiswa::class.java,
+                    "siswa_database"
+                ).build().also { INSTANCE = it }
+            }
         }
     }
 }
