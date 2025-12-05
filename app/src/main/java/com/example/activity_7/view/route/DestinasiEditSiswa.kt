18e729b7271a6069375beaca0c36872fb708fba6
Afnan Asilah <afnanyogs@gmail.com>
2025-12-05 12:37:24 +0700

    Menyediakan routeWithArgs untuk navigasi dinamis

--- app/src/main/java/com/example/activity_7/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/activity_7/view/route/DestinasiEditSiswa.kt
@@ -3,6 +3,8 @@ package com.example.activity_7.view.route
 import com.example.activity_7.R
 
 object DestinasiEditSiswa : DestinasiNavigasi {
-
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
