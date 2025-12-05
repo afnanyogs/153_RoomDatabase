da2edb85d0da47467b81feb852b0213fd623f8da
Afnan Asilah <afnanyogs@gmail.com>
2025-12-05 12:20:45 +0700

    Menambahkan NavHost dengan startDestination Home

--- app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
@@ -23,5 +23,9 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
 
+
+    }
 }
