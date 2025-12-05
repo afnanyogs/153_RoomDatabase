57e56c2648001132f2fdf6b72d38263565a3bae0
Afnan Asilah <afnanyogs@gmail.com>
2025-12-05 12:34:16 +0700

    Integrasi resource title untuk layar detail siswa

--- app/src/main/java/com/example/activity_7/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/activity_7/view/route/DestinasiDetailSiswa.kt
@@ -3,7 +3,10 @@ package com.example.activity_7.view.route
 import com.example.activity_7.R
 
 object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
 
+    const val itemIdArg = "idSiswa"
 
     val routeWithArgs = "$route/{$itemIdArg}"
 }
