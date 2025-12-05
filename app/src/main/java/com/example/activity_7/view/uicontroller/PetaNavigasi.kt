635d7950c03e004f1fcaaa46c23595af2d2005c3
Afnan Asilah <afnanyogs@gmail.com>
2025-12-05 12:20:05 +0700

    Implementasi navigasi dari Home ke EntrySiswa

--- app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
@@ -17,3 +17,11 @@ fun SiswaApp(navController: NavHostController = rememberNavController(), modifie
     HostNavigasi(navController = navController)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+
+}
