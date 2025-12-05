4cd244391b90001b8c7e6560059c90fddf967f37
Afnan Asilah <afnanyogs@gmail.com>
2025-12-05 12:21:39 +0700

    Memperbaiki parameter Modifier pada SiswaApp

--- app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
@@ -25,7 +25,14 @@ fun HostNavigasi(
 ){
     NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
     {
-
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
 
     }
 }
