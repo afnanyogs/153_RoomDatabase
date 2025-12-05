89440ab7fd90b449cd84dceacf596fa75c038def
Afnan Asilah <afnanyogs@gmail.com>
2025-12-05 12:17:04 +0700

    Menambahkan struktur navigasi utama aplikasi siswa

--- app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/activity_7/view/uicontroller/PetaNavigasi.kt
@@ -17,22 +17,3 @@ fun SiswaApp(navController: NavHostController = rememberNavController(), modifie
     HostNavigasi(navController = navController)
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HostNavigasi(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-){
-    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
-    {
-        composable(DestinasiHome.route){
-            HomeScreen(
-                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
-            )
-        }
-        composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
-        }
-
-    }
-}
